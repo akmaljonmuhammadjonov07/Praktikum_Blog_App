28a695afb7c24f050616b7b841272cc7057509a9
akmaljon <akmal@gmail.com>
2024-11-21 18:42:47 +0500

    All pages

--- components/shared/mode-toggle.tsx
+++ components/shared/mode-toggle.tsx
@@ -1,15 +1,15 @@
-'use client';
+'use client'
 
-import { Moon, Sun } from 'lucide-react';
-import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
-import { Button } from '../ui/button';
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+import { Button } from '../ui/button'
 
 function ModeToggle() {
-	const [mount, setMount] = useState(false);
-	const { setTheme, resolvedTheme } = useTheme();
+	const [mount, setMount] = useState(false)
+	const { setTheme, resolvedTheme } = useTheme()
 
-	useEffect(() => setMount(true), []);
+	useEffect(() => setMount(true), [])
 
 	return mount && resolvedTheme === 'dark' ? (
 		<Button size={'icon'} variant={'ghost'} onClick={() => setTheme('light')}>
@@ -19,7 +19,7 @@ function ModeToggle() {
 		<Button size={'icon'} onClick={() => setTheme('dark')} variant={'ghost'}>
 			<Moon />
 		</Button>
-	);
+	)
 }
 
-export default ModeToggle;
+export default ModeToggle
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,17 @@
-import React from 'react';
-
 export interface ChildProps {
 	children: React.ReactNode;
+}
+
+export interface IBlog {
+	title: string;
+	description: string;
+	author: string;
+	date: string;
+	image: string;
+	tags: string[];
+}
+
+export interface IAuthor {
+	name: string;
+	image: string;
 }
