2bc92420a0cc17c5860e1805f3f5223ef2f1cd28
akmaljon <akmal@gmail.com>
2024-11-21 14:40:39 +0500

    Layout. Navbar & Footer

---
+++ app/(root)/_components/mobile.tsx
@@ -0,0 +1,50 @@
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { navLinks } from '@/constants';
+import { cn } from '@/lib/utils';
+import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog';
+import { Menu } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+function Mobile() {
+	const pathname = usePathname();
+
+	return (
+		<div>
+			<Sheet>
+				<SheetTrigger asChild className='flex md:hidden'>
+					<Button size={'icon'} variant={'ghost'}>
+						<Menu />
+					</Button>
+				</SheetTrigger>
+				<SheetContent side={'left'}>
+					<Link href={'/'}>
+						<h1 className='text-4xl font-creteRound'>Sammi</h1>
+					</Link>
+					<Separator className='my-3' />
+					<DialogTitle />
+					<DialogDescription />
+					<div className='flex flex-col space-y-3'>
+						{navLinks.map(nav => (
+							<Link
+								key={nav.route}
+								href={nav.route}
+								className={cn(
+									'hover:bg-blue-400/20 py-2 px-3 cursor-pointer rounded-sm transition-colors flex items-center gap-2',
+									pathname === nav.route && 'text-blue-400 bg-blue-400/20'
+								)}
+							>
+								<nav.icon className='w-5 h-5' />
+								{nav.name}
+							</Link>
+						))}
+					</div>
+				</SheetContent>
+			</Sheet>
+		</div>
+	);
+}
+
+export default Mobile;
